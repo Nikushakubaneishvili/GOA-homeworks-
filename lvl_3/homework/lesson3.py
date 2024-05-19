55c43d8ccdc9c09a1233da1475c89e7f20eaef70
Nikusha.kubaneishvili <groovenick707@gmail.com>
2024-05-19 13:28:52 +0400

    lesson3

---
+++ lvl_3/homework/lesson3.py
@@ -0,0 +1,19 @@
+print (5 + 5) 
+print (10 - 5) 
+print (15 * 5) 
+print (100 / 5) 
+
+#creating a variables
+
+num1 = 6  #integer 
+num2 = 8 
+
+print(num1 + num2) 
+
+
+num2 = "10"  #string
+num3 = "1"  
+
+print(num2 + num3) 
+
+
