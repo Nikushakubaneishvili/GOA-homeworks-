c55f67a957110b2c1789828348d8d8850650c5b5
Nikusha.kubaneishvili <groovenick707@gmail.com>
2024-05-28 12:26:41 +0400

    GOA

---
+++ lvl_5/homework/lesson5.py
@@ -0,0 +1,41 @@
+#creating a 10 variables with every operation (+, -, * , /)
+ 
+num1 = 5 
+num2 = 10
+num3 = 8
+num4 = 6 
+num5 = 1
+num6 = 4 
+num7 = 3
+num8 = 7 
+num9 = 9 
+num10 = 2
+
+print(num1 + num2)
+print(num3 + num4)
+print(num5 + num6) 
+print (num7 + num8)
+print(num9 + num10)
+
+
+print(num1 - num2)
+print(num3 - num4)
+print(num5 - num6) 
+print (num7 - num8)
+print(num9 - num10)
+
+
+print(num1 * num2)
+print(num3 * num4)
+print(num5 * num6) 
+print (num7 * num8)
+print(num9 * num10)
+
+
+print(num1 / num2)
+print(num3 / num4)
+print(num5 / num6) 
+print (num7 / num8)
+print(num9 / num10)
+
+
---
+++ lvl_5/homework/lvl5.py
@@ -0,0 +1,14 @@
+name = "nika" 
+print(name)
+
+surname = "kubaneishvili"
+print(surname)
+
+age = 16
+print(age)
+
+gender = "male"
+print(gender)
+
+adress = "gldani"
+print(adress)
