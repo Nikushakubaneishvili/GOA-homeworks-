b3e6f65146be369d481f72d64ff29ec5552dee22
Nikusha.kubaneishvili <groovenick707@gmail.com>
2024-05-28 21:08:51 +0400

    lvl_6 classwork2

---
+++ lvl_6/classwork/level6.py
@@ -0,0 +1,9 @@
+firstname = input("please enter your firstname: ")
+lastname = input("please enter your lastname: ")
+age = input("please enter your age: ")
+gmail = input ("please enter your gmail: ")
+ 
+print("your information is : " + firstname + " " + lastname + " " + age + " " + gmail)  
+
+
+
