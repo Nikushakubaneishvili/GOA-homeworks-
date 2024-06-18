558c2c7df07374b81626fffed75d0e8abace55b3
Nikusha.kubaneishvili <groovenick707@gmail.com>
2024-06-18 14:04:17 +0400

    lesson10

---
+++ lvl_10/classwork/lesson10.py
@@ -0,0 +1,12 @@
+
+# 2 სახის ლუპი for / while loop
+# for loop
+# i არის საიტერაციო ცვლდი iteration / განმეორება
+# i in range(100) / ০৩১০১ 35mm 100x16
+# indentation 4 space (tab) / iteration განმეორება
+# იტერაცია იწყება 0 დან და არის integer
+
+for i in range(101) : 
+
+ print("I love you qutaisi")   
+ 
---
+++ lvl_10/homework/lesson10.py
@@ -0,0 +1,47 @@
+#first for loop
+for i in range(101) : 
+
+ print("I love you qutaisi") 
+
+#second for loop
+for i in range(90) : 
+
+ print("hello world")
+
+#third for loop
+for i in range(9) : 
+ print(10 + 5) 
+
+
+#fourth for loop
+for i in range(3): 
+ print(5 + 9)
+
+#fifth for loop 
+for i in range(3 + 8): 
+ print(i) 
+
+#sixth for loop
+for i in range (7): 
+  print("me mqvia nika")    
+
+#seventh for loop
+for i in range(1,11):
+  print(i) 
+
+
+#eighth for loop
+for i in range(1, 11, 2):
+ print(i) 
+
+#ninth for loop
+for i in range(1, 11, 3):
+ print(i)
+
+
+#tenth for loop
+num1=3
+for i in range(10):
+ print(num1)
+ num1 += 3 
+
